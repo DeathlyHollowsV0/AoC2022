b291518e2a51ad8a7623d86bd64d3a5b51d5ea81
DeathlyHollowsV0 <119601915+DeathlyHollowsV0@users.noreply.github.com>
2022-12-04 14:14:00 +0100

    Add files via upload

---
+++ AoC4.py
@@ -0,0 +1,11 @@
+pair = []
+numpairs = 0
+with open('day4.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        pair.append(line.strip("\n").replace(
+            "-", " ").replace(",", " ").split())
+for line in pair:
+    if ((int(line[0]) <= int(line[2])) and (int(line[1]) >= int(line[3]))) or ((int(line[0]) >= int(line[2])) and (int(line[1]) <= int(line[3]))):
+        numpairs = numpairs + 1
+print(numpairs)
